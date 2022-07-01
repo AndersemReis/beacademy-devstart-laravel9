c84afd490f6078560337faf9f57434f86a79c2d6
Andersem <andersem.reis@gmail.com>
2022-06-30 22:13:17 -0400

    atualizando migrations

--- aulas-beacademy/database/migrations/2022_07_01_020832_create_addresses_table.php
+++ aulas-beacademy/database/migrations/2022_07_01_020832_create_addresses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('address', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('address');
+        Schema::dropIfExists('addresses');
     }
 };
