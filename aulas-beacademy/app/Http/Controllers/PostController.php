7b4cfbeb7c58c8ee5a40779bbba70c28a8bd92ed
Andersem <andersem.reis@gmail.com>
2022-07-12 22:23:42 -0400

    Relacionamentos parte 1

--- aulas-beacademy/app/Http/Controllers/PostController.php
+++ aulas-beacademy/app/Http/Controllers/PostController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    protected $user;
+    protected $post;
+
+    public function __construct(User $user, Post $post)
+    {
+        $this->user = $user;
+        $this->post = $post;
+    }
+
+    public function index()
+    {
+        
+        $posts = $this->post->all();
+
+        return view('posts.index', compact('posts'));
+        
+    }
 }
--- aulas-beacademy/routes/web.php
+++ aulas-beacademy/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ViaCepController;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
